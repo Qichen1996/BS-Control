d9d435c48d1055d1d646ec99307c901252a1c047
Qichen1996 <qichenw@kth.se>
2024-04-08 12:55:57 +0000

    update results

--- simulate.sh
+++ simulate.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-acc=2000
+acc=3000
 
 for seed in 1 2 3; do
 for S in B; do
     for w in 12; do
         ./simulate.py -S $S --w_qos $w --seed $seed -a $acc $@
     done
-    w=12
-    ./simulate.py -S $S -A fixed --seed $seed -a $acc $@
+    # w=12
+    # ./simulate.py -S $S -A fixed --seed $seed -a $acc $@
     # ./simulate.py -S $S -A simple1 --seed $seed -a $acc $@
-    ./simulate.py -S $S -A simple1 --no_offload --seed $seed -a $acc $@
+    # ./simulate.py -S $S -A simple1 --no_offload --seed $seed -a $acc $@
     # for max_s in 1 2; do  # max depth of sleep
     #     ./simulate.py -S $S --w_qos $w --seed $seed -a $acc --max_sleep $max_s $@
     # done
